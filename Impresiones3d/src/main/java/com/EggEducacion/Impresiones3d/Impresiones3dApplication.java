e3e3012a6e4bac1e30c356cb27eff6220df98aac
Dannirc <37722439+Dannirc@users.noreply.github.com>
2021-10-26 18:27:00 -0300

    Inicio del proyecto

---
+++ Impresiones3d/src/main/java/com/EggEducacion/Impresiones3d/Impresiones3dApplication.java
@@ -0,0 +1,13 @@
+package com.EggEducacion.Impresiones3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Impresiones3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Impresiones3dApplication.class, args);
+	}
+
+}
---
+++ Impresiones3d/src/test/java/com/EggEducacion/Impresiones3d/Impresiones3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EggEducacion.Impresiones3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Impresiones3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
